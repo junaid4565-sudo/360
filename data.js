da592c3f9e5cbc215ba23b6f4dc1560825cf6e47
junaid4565-sudo <junaidarafath5@gmail.com>
2025-05-28 21:00:21 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,105 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-03",
+      "name": "03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.01467849243390873,
+        "pitch": 0.004424278437269891,
+        "fov": 1.0764170620549
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09520187677136782,
+          "pitch": 0.07214528276176324,
+          "rotation": 0,
+          "target": "1-01"
+        },
+        {
+          "yaw": 0.7320126199314849,
+          "pitch": 0.05105538025815015,
+          "rotation": 0,
+          "target": "2-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-01",
+      "name": "01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-02",
+      "name": "02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
